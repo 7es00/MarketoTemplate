023e4fe273627c3366d7e0ac70aefccfd05faeb8
Mohammed Mostafa <mmoustafa789@gmail.com>
2025-05-12 05:47:35 +0300

    Tes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
+checkNavbar();
 function toggleMenu() {
-    const navLinks = document.getElementById('navMenue');
-    if (navLinks.style.display === 'flex') {
-        navLinks.style.display = 'none';
-    } else {
+    document.querySelector('.nav-links-wrapper').classList.toggle('active');
+}
+
+function checkNavbar() {
+    const burger = document.getElementById('burger');
+    const navLinks = document.querySelector('.nav-links-wrapper');
+    const burgerDisplay = window.getComputedStyle(burger).display;
+
+    if (burgerDisplay === 'none') {
+        navLinks.classList.remove('active');
         navLinks.style.display = 'flex';
+    } else {
+        navLinks.classList.remove('active');
+        navLinks.style.display = 'none';
     }
 }
+
+window.addEventListener('resize', checkNavbar);
