87b17939e0604bf544cae6597626bcc903307171
Mohammed Mostafa <mmoustafa789@gmail.com>
2025-05-12 05:27:17 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navLinks = document.getElementById('navMenue');
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+}
